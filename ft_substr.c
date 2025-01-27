7e19fbc74a83b2c2578414fbcdf1def5dd77c43b
Tigran Baghdasaryan <tbaghdas@c1r6s3.42yerevan.am>
2025-01-28 00:43:54 +0400

    Added more functions

---
+++ ft_substr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbaghdas <tbaghdas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 21:32:10 by tbaghdas          #+#    #+#             */
+/*   Updated: 2025/01/27 21:49:29 by tbaghdas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub_s;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	sub_s = (char *) malloc((len + 1) * sizeof(char));
+	if (!sub_s)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		sub_s[i] = s[start + i];
+		i++;
+	}
+	sub_s[i] = '\0';
+	return (sub_s);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tbaghdas <tbaghdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 23:03:47 by tbaghdas          #+#    #+#             */
-/*   Updated: 2025/01/24 23:52:22 by tbaghdas         ###   ########.fr       */
+/*   Updated: 2025/01/27 22:36:07 by tbaghdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,13 @@ int		ft_isalpha(int s);
 int		ft_isdigit(int d);
 int		isascii(int c);
 int		ft_isprint(int c);
-size_t	ft_strlen(char *str);
+size_t	ft_strlen(const char *str);
 void	*ft_memset(void *ptr, int value, size_t num);
 void	ft_bzero(void *ptr, size_t num);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
