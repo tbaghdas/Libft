2d5ef686ed97c6b05d2709fa66daa4c688cd8eea
tbaghdas <patkanyan1@gmail.com>
2025-02-15 15:56:24 +0400

    Fix last issue

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: tbaghdas <tbaghdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 23:40:27 by tbaghdas          #+#    #+#             */
-/*   Updated: 2025/02/13 21:22:51 by tbaghdas         ###   ########.fr       */
+/*   Updated: 2025/02/15 15:52:21 by tbaghdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,20 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*node;
+	void	*new_cont;
 
 	if (!lst || !f || !del)
 		return (NULL);
+	new_cont = NULL;
 	new_list = NULL;
 	while (lst)
 	{
-		node = ft_lstnew(f(lst -> content));
+		new_cont = f(lst -> content);
+		node = ft_lstnew(new_cont);
 		if (!node)
 		{
 			ft_lstclear(&new_list, del);
+			del(new_cont);
 			return (NULL);
 		}
 		ft_lstadd_back(&new_list, node);
